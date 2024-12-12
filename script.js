098eefeb8066022a1ed65e5757793076f2d5da50
aisalkyn4160 <aisalkyn.4160@gmail.com>
2024-12-12 23:42:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const header = document.querySelector(".header");
+const burger = document.querySelector(".burger");
+
+
+burger.addEventListener("click", () => {
+    header.classList.toggle("mobile-header");
+    if (header.classList.contains("mobile-header")) {
+        document.body.classList.add("no-scroll");
+    } else {
+        document.body.classList.remove("no-scroll");
+    }
+})
